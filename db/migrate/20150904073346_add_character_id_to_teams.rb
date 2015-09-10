522096c2aef52db6c901c28148cf3d4a6494a8e6
Thedischman <milesthedisch@gmail.com>
2015-09-11 09:07:04 +1000

    Please

--- db/migrate/20150904073346_add_character_id_to_teams.rb
+++
@@ -1,5 +0,0 @@
-class AddCharacterIdToTeams < ActiveRecord::Migration
-  def change
-  	  	add_column :teams, :character_id, :integer
-  end
-end
--- db/migrate/20150904080303_add_team_id_to_characters.rb
+++
@@ -1,5 +0,0 @@
-class AddTeamIdToCharacters < ActiveRecord::Migration
-  def change
-  	add_column :characters, :team_id, :integer
-  end
-end
