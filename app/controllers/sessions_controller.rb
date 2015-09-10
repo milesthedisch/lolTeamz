e8474b7c3ccbd878482d81c3ffe05896a45c070c
Thedischman <milesthedisch@gmail.com>
2015-09-11 09:20:20 +1000

    Stop prying into my business

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by :username => params[:username]
     if user.present? && user.authenticate(params[:password])
-      binding.pry
+
       session[:user_id] = user.id
       flash[:message] = "Welcome back, #{ user.username }"
       redirect_to root_path
